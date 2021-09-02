5deba65c1872f039be9a4d193c32e932ba45078b
ffdietz <felipefierrod@gmail.com>
2021-09-02 03:23:53 +0200

    select record/pause with icon

--- include/microSD.h
+++ include/microSD.h
@@ -2,8 +2,7 @@
  
 File myFile;
  
-void SD_init()
-{
+void sd_init(){
   Serial.begin(9600);
   Serial.print("Initializing SD card...");
   // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
@@ -19,18 +18,17 @@ void SD_init()
   Serial.println("initialization done."); 
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-    myFile = SD.open("test.txt", FILE_WRITE);
+    myFile = SD.open("hidroscopia.txt", FILE_WRITE);
 }
 
 
-void SD_write(uint16_t input_write)
-{
+void sd_write(uint16_t input_write){
   if (myFile) {
     myFile.println(input_write);
     myFile.flush();
   }
 }
 
-void SD_stop(){  
+void sd_stop(){  
     myFile.close(); 
 }
